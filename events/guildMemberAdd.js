995faea13dfb1f9e5e448a96315313099cb8d829
Xurxo Martínez Ferreira <xurxolaxe@gmail.com>
2023-05-19 00:03:33 +0200

    arreglado error con roles ao unirse

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,7 +2,13 @@ const { Events } = require("discord.js");
 
 module.exports = {
     name: Events.GuildMemberAdd,
-    async execute(client, user) {
-        member.roles.add(["864649993453174805", "864650641167351818", "726143285545926736", "898862006852009985", "864656505541623849"]);
+    async execute(client, member) {
+        member.roles.add([
+            "864649993453174805",
+            "864650641167351818",
+            "726143285545926736",
+            "898862006852009985",
+            "864656505541623849",
+        ]);
     },
 };
