759cc3f130f1ff95e0d18e9305d32d1f0f5f834f
Xurxo Martínez Ferreira <xurxolaxe@gmail.com>
2023-05-19 00:06:42 +0200

    solucionado erro caracruz.js

--- commands/utilidad/caracruz.js
+++ commands/utilidad/caracruz.js
@@ -1,11 +1,21 @@
-const { SlashCommandBuilder, MessageEmbed } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("caracruz")
         .setDescription("Juega al cara o cruz.")
-        .addStringOption((str1) => str1.setName("cara").setDescription("Cara de la moneda.").setRequired(true))
-        .addStringOption((str2) => str2.setName("cruz").setDescription("Cruz de la moneda.").setRequired(true)),
+        .addStringOption((str1) =>
+            str1
+                .setName("cara")
+                .setDescription("Cara de la moneda.")
+                .setRequired(true)
+        )
+        .addStringOption((str2) =>
+            str2
+                .setName("cruz")
+                .setDescription("Cruz de la moneda.")
+                .setRequired(true)
+        ),
     async execute(interaction) {
         const cara = interaction.options.getString("cara", true);
         const cruz = interaction.options.getString("cruz", true);
@@ -13,7 +23,13 @@ module.exports = {
         const caracruz = [`**Cara** (${cara})`, `**Cruz** (${cruz})`];
         const resultado = caracruz[Math.floor(Math.random() * caracruz.length)];
 
-        const embed = new MessageEmbed().setColor("#a30584").addFields({ name: "Cara:", value: cara, inline: true }, { name: "Cruz:", value: cruz, inline: true }).addField("Ganador:", resultado);
+        const embed = new EmbedBuilder()
+            .setColor("#a30584")
+            .addFields(
+                { name: "Cara:", value: cara, inline: true },
+                { name: "Cruz:", value: cruz, inline: true }
+            )
+            .addField("Ganador:", resultado);
 
         interaction.reply({ embeds: [embed] });
     },
