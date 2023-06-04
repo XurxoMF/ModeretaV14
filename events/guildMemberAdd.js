342208f76d0ed288258e622832ad8b80691c6e14
Xurxo Martínez Ferreira <xurxolaxe@gmail.com>
2023-06-04 21:54:40 +0200

    error avatar bienvenida

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,7 +4,7 @@ module.exports = {
     name: Events.GuildMemberAdd,
     async execute(client, db, member) {
         const id = member.id;
-        const avatar = member.avatarURL;
+        const avatar = member.displayAvatarURL();
 
         const bienvenida = new EmbedBuilder()
             .setTitle("BIENVENID@ A ASTRO VAPORWAVE")
@@ -13,7 +13,8 @@ module.exports = {
                 \nPuedes personalizar tus roles en <id:customize> y ocultar y mostrar canales a tu gusto en <id:browse>`
             )
             .setThumbnail(avatar)
-            .setImage("https://i.postimg.cc/Bb11vF6q/fondo-saludo.gif");
+            .setImage("https://i.postimg.cc/Bb11vF6q/fondo-saludo.gif")
+            .setColor("#ff00ff");
 
         await client.channels.cache.get("726150826778689566").send({
             embeds: [bienvenida],
