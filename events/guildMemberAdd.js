d6be114b3669bca9dea8b88ba4ede76f67a07fb0
Xurxo Martínez Ferreira <xurxolaxe@gmail.com>
2023-06-04 19:24:34 +0200

    saludos

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,25 @@
-const { Events } = require("discord.js");
+const { Events, EmbedBuilder } = require("discord.js");
 
 module.exports = {
     name: Events.GuildMemberAdd,
     async execute(client, db, member) {
-        member.roles.add([
+        const id = member.id;
+        const avatar = member.avatarURL;
+
+        const bienvenida = new EmbedBuilder()
+            .setTitle("BIENVENID@ A ASTRO VAPORWAVE")
+            .setDescription(
+                `<@${id}> te damos la bienvenida a Astro Munidad. 
+                \nPuedes personalizar tus roles en <id:customize> y ocultar y mostrar canales a tu gusto en <id:browse>`
+            )
+            .setThumbnail(avatar)
+            .setImage("https://i.postimg.cc/Bb11vF6q/fondo-saludo.gif");
+
+        await client.channels.cache.get("726150826778689566").send({
+            embeds: [bienvenida],
+        });
+
+        await member.roles.add([
             "864649993453174805",
             "864650641167351818",
             "726143285545926736",
