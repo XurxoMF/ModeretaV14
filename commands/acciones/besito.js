9a71a57c7c9abb4d4a8d21d0e8b246d6523ba5af
Xurxo Martínez Ferreira <xurxolaxe@gmail.com>
2023-06-04 12:13:32 +0200

    Omedetou y betiso(acciones)

---
+++ commands/acciones/besito.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    uso: " `<usuario>`",
+    data: new SlashCommandBuilder()
+        .setName("besito")
+        .setDescription(
+            "Le das un besito en la frente o la mejilla a la persona a la que menciones."
+        )
+        .addMentionableOption((mention) =>
+            mention
+                .setName("usuario")
+                .setDescription("Usuario al que darle un besito.")
+                .setRequired(true)
+        ),
+    async execute(client, db, interaction) {
+        const userAct = interaction.options.getMentionable("usuario");
+        const userSend = interaction.member;
+
+        if (userSend.id === userAct.id) {
+            interaction.reply({
+                content: "Anda que no tiene que ser difícil darte un beso a ti mismo ^^",
+            });
+        }
+
+        const gifs = [
+            "https://i.postimg.cc/RVZf7WPR/1.gif",
+            "https://i.postimg.cc/TPgWdgwR/2.gif",
+            "https://i.postimg.cc/t41xcLGn/3.gif",
+            "https://i.postimg.cc/85GfjcJL/4.gif",
+            "https://i.postimg.cc/Z51dzVJ7/5.gif",
+            "https://i.postimg.cc/YqDFbtZw/6.gif",
+        ];
+
+        const embed = new EmbedBuilder()
+            .setColor("#a30584")
+            .setDescription(`${userSend} le ha dado un besito a ${userAct} UwU`)
+            .setImage(gifs[Math.floor(Math.random() * gifs.length)]);
+
+        interaction.reply({
+            embeds: [embed],
+        });
+    },
+};
---
+++ commands/acciones/omedetou.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    uso: " `<usuario>`",
+    data: new SlashCommandBuilder()
+        .setName("omedetou")
+        .setDescription("Omedetou Shinji 👏👏")
+        .addMentionableOption((mention) =>
+            mention.setName("usuario").setDescription("Usuario al felicitar.").setRequired(true)
+        ),
+    async execute(client, db, interaction) {
+        const userAct = interaction.options.getMentionable("usuario");
+        const userSend = interaction.member;
+
+        if (userSend.id === userAct.id) {
+            interaction.reply({
+                content: "Felicita a otro usuario no a ti ^^",
+            });
+        }
+
+        const embed = new EmbedBuilder()
+            .setColor("#a30584")
+            .setDescription(`Omedetou ${userAct}! 👏`)
+            .setImage("https://i.postimg.cc/X7PZ9QBj/Omedetou.gif");
+
+        interaction.reply({
+            embeds: [embed],
+        });
+    },
+};
