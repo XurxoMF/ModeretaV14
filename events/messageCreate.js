0416b770efc78f88788451987e542fffec8fd6b6
Xurxo Martínez Ferreira <xurxolaxe@gmail.com>
2023-07-12 09:47:51 +0200

    .

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -39,164 +39,198 @@ module.exports = {
         }
         // END MÚSICA
 
-        // SERIES USERS DROP
+        // PING DROPS SOFI
+        if (
+            message.channel.id === "1101853797573206016" &&
+            message.author.id === "1128578188503683092"
+        ) {
+            const embed = message.embeds.length >= 1 ? message.embeds[0] : undefined;
+
+            if (embed !== undefined) {
+                if (embed.title === "Captcha Drop") {
+                    message.reply({
+                        content: `<@&1096410227408121898> **Captcha Drop** disponible!`,
+                    });
+                } else if (embed.title === "SOFI: MINIGAME") {
+                    message.reply({
+                        content: `<@&1096410227408121898> **Minijuego** desponible!`,
+                    });
+                } else if (
+                    embed.description.includes("I will drop cards from the most voted series")
+                ) {
+                    message.reply({
+                        content: `<@&1096410227408121898> **Drop de Series** desponible!`,
+                    });
+                }
+            }
+        }
+
+        // SERIES DROP
         //Sofu = 950166445034188820
         //Gio = 556249326951727115
-        // if (message.author.id === "556249326951727115") {
-        //     const msgCont = message.content;
+        if (message.author.id === "950166445034188820") {
+            const msgCont = message.content;
 
-        //     console.log(msgCont);
+            console.log(msgCont);
 
-        //     let series = [];
-        //     let userIds = new Set();
+            let series = [];
+            let userIds = new Set();
 
-        //     if (msgCont.startsWith("- ")) {
+            if (
+                message.author.id === "950166445034188820" &&
+                message.content.startsWith("> ") &&
+                message.content.includes("has dropeado estas cartas!")
+            ) {
+                const frases = message.content.split("\n");
+                for (const frase of frases) {
+                    if (frase.startsWith("> ")) {
+                        series.push(frase.split(" • ")[2]);
+                    }
+                }
+            }
 
-        //     }
+            // if (message.content.includes("")) {
+            //     // drop normal SOFU
+            //     const frases = message.content.split("\n");
+            //     for (let i = 1; i < frases.length; i++) {
+            //         series.push(frases[i].split(" • ")[2].slice(1, -1));
+            //     }
+            // } else if (
+            //     message.content.includes("**") &&
+            //     message.content.startsWith("`1]`") &&
+            //     message.content.includes("ɢ")
+            // ) {
+            //     // drop de char-serie por actividade
+            //     const lineas = message.content.split("\n");
+            //     for (const linea of lineas) {
+            //         series.push(linea.split("•")[4].trim());
+            //     }
+            // } else if (
+            //     message.content.includes("**") &&
+            //     message.content.startsWith("`1]`") &&
+            //     !message.content.includes("ɢ")
+            // ) {
+            //     // drop de char-serie por actividade sin g
+            //     const lineas = message.content.split("\n");
+            //     for (const linea of lineas) {
+            //         series.push(linea.split("•")[3].trim());
+            //     }
+            // } else if (!message.content.includes("**") && message.content.startsWith("`1]`")) {
+            //     // drop de serie por actividade
+            //     const lineas = message.content.split("\n");
+            //     for (const linea of lineas) {
+            //         series.push(linea.split("•")[2].trim());
+            //     }
+            // }
 
-        //if (message.content.includes("")) {
-        //     // drop normal SOFU
-        //     const frases = message.content.split("\n");
-        //     for (let i = 1; i < frases.length; i++) {
-        //         series.push(frases[i].split(" • ")[2].slice(1, -1));
-        //     }
-        // } else if (
-        //     message.content.includes("**") &&
-        //     message.content.startsWith("`1]`") &&
-        //     message.content.includes("ɢ")
-        // ) {
-        //     // drop de char-serie por actividade
-        //     const lineas = message.content.split("\n");
-        //     for (const linea of lineas) {
-        //         series.push(linea.split("•")[4].trim());
-        //     }
-        // } else if (
-        //     message.content.includes("**") &&
-        //     message.content.startsWith("`1]`") &&
-        //     !message.content.includes("ɢ")
-        // ) {
-        //     // drop de char-serie por actividade sin g
-        //     const lineas = message.content.split("\n");
-        //     for (const linea of lineas) {
-        //         series.push(linea.split("•")[3].trim());
-        //     }
-        // } else if (!message.content.includes("**") && message.content.startsWith("`1]`")) {
-        //     // drop de serie por actividade
-        //     const lineas = message.content.split("\n");
-        //     for (const linea of lineas) {
-        //         series.push(linea.split("•")[2].trim());
-        //     }
-        // }
+            if (series.length > 0) {
+                // busca os usuarios que coleccionan as series dropeadas
+                const res = await db.SeriesUsers.findAll({
+                    where: {
+                        serie: {
+                            [db.Sequelize.Op.or]: [
+                                db.sequelize.where(
+                                    db.sequelize.fn("LOWER", db.sequelize.col("serie")),
+                                    "LIKE",
+                                    series[0]?.toLowerCase()
+                                ),
+                                db.sequelize.where(
+                                    db.sequelize.fn("LOWER", db.sequelize.col("serie")),
+                                    "LIKE",
+                                    series[1]?.toLowerCase()
+                                ),
+                                db.sequelize.where(
+                                    db.sequelize.fn("LOWER", db.sequelize.col("serie")),
+                                    "LIKE",
+                                    series[2]?.toLowerCase()
+                                ),
+                            ],
+                        },
+                    },
+                });
 
-        //     if (series.length > 0) {
-        //         // busca os usuarios que coleccionan as series dropeadas
-        //         const res = await db.SeriesUsers.findAll({
-        //             where: {
-        //                 serie: {
-        //                     [db.Sequelize.Op.or]: [
-        //                         db.sequelize.where(
-        //                             db.sequelize.fn("LOWER", db.sequelize.col("serie")),
-        //                             "LIKE",
-        //                             series[0]?.toLowerCase()
-        //                         ),
-        //                         db.sequelize.where(
-        //                             db.sequelize.fn("LOWER", db.sequelize.col("serie")),
-        //                             "LIKE",
-        //                             series[1]?.toLowerCase()
-        //                         ),
-        //                         db.sequelize.where(
-        //                             db.sequelize.fn("LOWER", db.sequelize.col("serie")),
-        //                             "LIKE",
-        //                             series[2]?.toLowerCase()
-        //                         ),
-        //                     ],
-        //                 },
-        //             },
-        //         });
+                const users = [...res];
 
-        //         const users = [...res];
+                for (const u of users) {
+                    const id = u.getDataValue("userId");
+                    userIds.add(id);
+                }
 
-        //         for (const u of users) {
-        //             const id = u.getDataValue("userId");
-        //             userIds.add(id);
-        //         }
+                for (const u of users) {
+                    const id = u.getDataValue("userId");
+                    userIds.add(id);
+                }
 
-        //         for (const u of users) {
-        //             const id = u.getDataValue("userId");
-        //             userIds.add(id);
-        //         }
+                // envía pings as persoas cas series na súa lista
+                if (userIds.size > 0) {
+                    let res = "";
+                    for (const uid of userIds) {
+                        res += `<@${uid}> `;
+                    }
 
-        //         // envía pings as persoas cas series na súa lista
-        //         if (userIds.size > 0) {
-        //             let res = "";
-        //             for (const uid of userIds) {
-        //                 res += `<@${uid}> `;
-        //             }
+                    const boton = new ButtonBuilder()
+                        .setCustomId("lista_series")
+                        .setLabel("📑")
+                        .setStyle(ButtonStyle.Primary);
+                    const row = new ActionRowBuilder().addComponents(boton);
 
-        //             const boton = new ButtonBuilder()
-        //                 .setCustomId("lista_series")
-        //                 .setLabel("📑")
-        //                 .setStyle(ButtonStyle.Primary);
-        //             const row = new ActionRowBuilder().addComponents(boton);
+                    const resMes = await message.reply({
+                        content: `${res}se dropearon cartas de tu lista!`,
+                        components: [row],
+                    });
 
-        //             const resMes = await message.reply({
-        //                 content: `${res}se dropearon cartas de tu lista!`,
-        //                 components: [row],
-        //             });
+                    const collector = resMes.createMessageComponentCollector({
+                        componentType: ComponentType.Button,
+                        idle: 50_000,
+                    });
 
-        //             const collector = resMes.createMessageComponentCollector({
-        //                 componentType: ComponentType.Button,
-        //                 idle: 50_000,
-        //             });
+                    collector.on("collect", async (i) => {
+                        // preparar obxeto de series
+                        const userSeries = {};
+                        for (const s of series) {
+                            userSeries[s] = [];
+                            for (const u of users) {
+                                if (u.getDataValue("serie").toLowerCase() === s.toLowerCase()) {
+                                    userSeries[s].push(u.getDataValue("userId"));
+                                }
+                            }
+                        }
 
-        //             collector.on("collect", async (i) => {
-        //                 // preparar obxeto de series
-        //                 const userSeries = {};
-        //                 for (const s of series) {
-        //                     userSeries[s] = [];
-        //                     for (const u of users) {
-        //                         if (u.getDataValue("serie").toLowerCase() === s.toLowerCase()) {
-        //                             userSeries[s].push(u.getDataValue("userId"));
-        //                         }
-        //                     }
-        //                 }
+                        // preparar embed
+                        const fields = [];
+                        for (const s in userSeries) {
+                            let line = ``;
+                            if (userSeries[s].length === 0) {
+                                line = `\`Sin usuarios\`, `;
+                            } else {
+                                for (const uid of userSeries[s]) {
+                                    line += `<@${uid}>, `;
+                                }
+                            }
+                            line = line.slice(0, -2);
+                            fields.push({ name: s, value: line });
+                        }
 
-        //                 // preparar embed
-        //                 const fields = [];
-        //                 for (const s in userSeries) {
-        //                     let line = ``;
-        //                     if (userSeries[s].length === 0) {
-        //                         line = `\`Sin usuarios\`, `;
-        //                     } else {
-        //                         for (const uid of userSeries[s]) {
-        //                             line += `<@${uid}>, `;
-        //                         }
-        //                     }
-        //                     line = line.slice(0, -2);
-        //                     fields.push({ name: s, value: line });
-        //                 }
+                        const embed = new EmbedBuilder()
+                            .setTitle("Series coleccionadas por cada usuario:")
+                            .setColor("#a30584")
+                            .addFields(...fields);
 
-        //                 const embed = new EmbedBuilder()
-        //                     .setTitle("Series coleccionadas por cada usuario:")
-        //                     .setColor("#a30584")
-        //                     .addFields(...fields);
-
-        //                 try {
-        //                     i.reply({
-        //                         embeds: [embed],
-        //                         ephemeral: true,
-        //                     });
-        //                 } catch (err) {
-        //                     i.reply({
-        //                         content: "Ha ocurrido un error, inténtalo de nuevo ^^",
-        //                         ephemeral: true,
-        //                     });
-        //                 }
-        //             });
-        //         }
-        //     }
-        // }
-        // END SERIES USERS DROP
+                        try {
+                            i.reply({
+                                embeds: [embed],
+                                ephemeral: true,
+                            });
+                        } catch (err) {
+                            i.reply({
+                                content: "Ha ocurrido un error, inténtalo de nuevo ^^",
+                                ephemeral: true,
+                            });
+                        }
+                    });
+                }
+            }
+        }
 
         // KARUTA DROP PING
         if (
--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -4,14 +4,15 @@ module.exports = {
     name: Events.MessageReactionAdd,
     async execute(client, db, reaction, user) {
         if (
-            reaction.message.channelId === "742804647894581289" &&
-            reaction.message.author.id === "646937666251915264" &&
-            reaction._emoji.name === "⭐" &&
-            reaction.count === 1 &&
-            (reaction.message.content.includes("is dropping 3 cards!") ||
-                reaction.message.content.includes(
-                    "This drop has expired and the cards can no longer be grabbed."
-                ))
+            (reaction.message.channelId === "742804647894581289" &&
+                reaction.message.author.id === "646937666251915264" &&
+                reaction._emoji.name === "⭐" &&
+                reaction.count === 1 &&
+                (reaction.message.content.includes("is dropping 3 cards!") ||
+                    reaction.message.content.includes(
+                        "This drop has expired and the cards can no longer be grabbed."
+                    ))) ||
+            reaction.message.content.includes("I'm dropping 3 cards")
         ) {
             const pogdrops = await client.channels.cache.get("831596499980779530");
 
